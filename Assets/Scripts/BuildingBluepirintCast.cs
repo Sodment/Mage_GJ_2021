ec3c5cb65854eab9b5908825b5a618189f684f42
Sodment <sodment@gmail.com>
2021-03-05 22:05:13 +0100

    Building

--- Assets/Scripts/BuildingBluepirintCast.cs
+++ Assets/Scripts/BuildingBluepirintCast.cs
@@ -8,14 +8,17 @@ public class BuildingBluepirintCast : MonoBehaviour
     RaycastHit hit;
     Vector3 movePoint;
     public GameObject buildingBlueprint;
+    public MeshRenderer myRenderer;
+    public Material buildingBlockedMaterial;
+    public Material buildingPossibleMaterial;
+    int layerMask = 1 << 6;
     // Start is called before the first frame update
     void Start()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray, out hit, 50000.0f))
+        if(Physics.Raycast(ray, out hit, 50000.0f, (1 << 0)))
         {
             transform.position = hit.point;
-            Debug.Log(transform.position);
         }
     }
 
@@ -23,13 +26,21 @@ public class BuildingBluepirintCast : MonoBehaviour
     void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        bool building_colliding = Physics.CheckBox(transform.position + new Vector3Int(0, 1, 0), new Vector3(4.0f, 0.0f, 4.0f), Quaternion.identity, layerMask);
         //Debug.DrawLine(ray.origin, ray.origin + ray.direction, Color.red);
-        if (Physics.Raycast(ray, out hit, 50000.0f))
+        if (Physics.Raycast(ray, out hit, 50000.0f, (1 << 0)))
         {
             transform.position = hit.point;
-            Debug.Log(transform.position);
         }
-        if (Input.GetMouseButtonDown(0))
+        if (building_colliding)
+        {
+            BuildingBlocked();
+        }
+        else
+        {
+            BuildingPossible();
+        }
+        if (Input.GetMouseButtonDown(0) && !building_colliding)
         {
             BuildBuilding();
         }
@@ -37,7 +48,17 @@ public class BuildingBluepirintCast : MonoBehaviour
 
     public void BuildBuilding()
     {
-        Instantiate(buildingBlueprint, transform.position, transform.rotation);
+        Instantiate(buildingBlueprint, transform.position + new Vector3Int(0, 1, 0), transform.rotation);
         Destroy(gameObject);
+    }
+
+    public void BuildingBlocked()
+    {
+        myRenderer.material = buildingBlockedMaterial;
+    }
+
+    public void BuildingPossible()
+    {
+        myRenderer.material = buildingPossibleMaterial;
     }
 }
